88d092e80c8a4253c9e2157df499f5ec36e5bbc8
st67137 <st67137@upce.cz>
2025-10-27 09:11:59 +0100

    Remove forbidden author tag and add JavaDoc to Parameter class

--- src/main/java/cz/upce/fei/nnptp/zz/entity/Parameter.java
+++ src/main/java/cz/upce/fei/nnptp/zz/entity/Parameter.java
@@ -8,27 +8,57 @@ package cz.upce.fei.nnptp.zz.entity;
 import java.time.LocalDateTime;
 
 /**
- * @author Roman
+ * Generic class representing a parameter that holds a value of any type.
+ * <p>
+ * Used for storing information such as titles, descriptions, websites, or expiration dates
+ * within password entries.
+ * </p>
+ *
+ * @param <T> the type of the value stored in this parameter
  */
 public class Parameter<T> {
 
     private T value;
-
+    /**
+     * Default constructor.
+     * Initializes a new parameter without a value.
+     */
     public Parameter() {
     }
 
+    /**
+     * Constructs a new parameter with the specified value.
+     *
+     * @param value the value to store in this parameter
+     */
     public Parameter(T value) {
         this.value = value;
     }
 
+    /**
+     * Returns the value stored in this parameter.
+     *
+     * @return the current value
+     */
     public T getValue() {
         return value;
     }
 
+    /**
+     * Sets the value of this parameter.
+     *
+     * @param value the new value to assign
+     */
     public void setValue(T value) {
         this.value = value;
     }
 
+    /**
+     * Contains standardized parameter names used across the application.
+     * <p>
+     * Examples include title, description, website, and expiration date.
+     * </p>
+     */
     public static class StandardizedParameters {
         public static final String TITLE = "title";
         public static final String EXPIRATION_DATETIME = "expiration-datetime";
