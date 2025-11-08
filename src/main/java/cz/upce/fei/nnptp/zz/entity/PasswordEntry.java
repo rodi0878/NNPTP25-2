f37f5cfb6283da33e0b0d6bc8bc04d971b9d6634
Saunic <Saunic@SAUNIC>
2025-11-08 17:36:26 +0100

    Add toString, equals and hashCode to PasswordEntry

--- src/main/java/cz/upce/fei/nnptp/zz/entity/PasswordEntry.java
+++ src/main/java/cz/upce/fei/nnptp/zz/entity/PasswordEntry.java
@@ -1,6 +1,7 @@
 package cz.upce.fei.nnptp.zz.entity;
 
 import java.util.HashMap;
+import java.util.Objects;
 
 public class PasswordEntry {
 
@@ -40,6 +41,41 @@ public class PasswordEntry {
 
     public Parameter<?> getParameter(String key) {
         return (parameters != null) ? parameters.get(key) : null;
+    }
+
+    @Override
+    public String toString() {
+        return "PasswordEntry{" + "id=" + id + ", password=" + password + ", parameters=" + parameters + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + this.id;
+        hash = 29 * hash + Objects.hashCode(this.password);
+        hash = 29 * hash + Objects.hashCode(this.parameters);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final PasswordEntry other = (PasswordEntry) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.password, other.password)) {
+            return false;
+        }
+        return Objects.equals(this.parameters, other.parameters);
     }
     
 }
